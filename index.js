d5d26af95856561d039e97492523a0c92c3ee278
Khubaibul Islam <khubaibulislam2001@gmail.com>
2022-11-24 14:06:37 +0600

    User Store To Database Complete

--- index.js
+++ index.js
@@ -22,6 +22,37 @@ app.get("/", (req, res) => {
     res.send("Deal Of The Day Server Is Running...")
 })
 
+
+// Database Connection Function
+async function dataBase() {
+    try {
+        const usersCollection = client.db("deals-of-the-day").collection("users");
+
+        // Save User Info
+        app.put("/user/:email", async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: user,
+            }
+
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
+
+            res.send({ result })
+        })
+
+    }
+    catch (err) {
+        console.log(err.message.bgRed.bold)
+        console.log(err.stack.bgBlue.bold)
+    }
+
+}
+
+dataBase().catch(err => console.log(err.bold.bgRed))
+
 // Listen
 app.listen(port, () => {
     console.log(`Server Is Running On ${port}`.bgCyan.bold);
